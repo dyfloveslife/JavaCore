f3e8dc51175c07c172237eb40d34341a2266dae6
dyfloveslife <786616590@qq.com>
2021-03-15 15:26:34 +0800

    New code: add program list 9-1.

---
+++ src/linkedList/LinkedListTest.java
@@ -0,0 +1,49 @@
+package linkedList;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+        List<String> a = new LinkedList<>();
+        a.add("Amy");
+        a.add("Carl");
+        a.add("Erica");
+
+        List<String> b = new LinkedList<>();
+        b.add("Bob");
+        b.add("Doug");
+        b.add("Frances");
+        b.add("Gloria");
+
+        // merge the words from b into a
+        ListIterator<String> aIter = a.listIterator();
+        Iterator<String> bIter = b.iterator();
+
+        while (bIter.hasNext()) {
+            if (aIter.hasNext()) aIter.next();
+            aIter.add(bIter.next());
+        }
+        System.out.println(a);
+
+        // remove every second word from b
+        bIter = b.iterator();
+        while (bIter.hasNext()) {
+            // skip one element
+            bIter.next();
+            if (bIter.hasNext()) {
+                // skip next element
+                bIter.next();
+                // remove that element
+                bIter.remove();
+            }
+        }
+
+        System.out.println(b);
+        // remove all words in b from a
+        a.remove(b);
+        System.out.println(a);
+    }
+}
